e44394e8b008f8f42dce80a9328bc71a6f3bd88c
Maria Eloiza Marques Consolino <165097868+mariaeloiza-consolino@users.noreply.github.com>
2025-09-17 23:36:58 +0000

    insert into de progresso, videoaulas, aluno

--- projeto-uniplanning/src/main/resources/import.sql
+++ projeto-uniplanning/src/main/resources/import.sql
@@ -3,3 +3,21 @@ insert into Professor (nome, cpf, email, senha) values ('Paulo Henrique', '987.6
 insert into Professor (nome, cpf, email, senha) values ('Ana Beatriz Costa', '111.222.333-44', 'ana.beatriz@exemplo.com', 'Abc!2025');
 insert into Professor (nome, cpf, email, senha) values ('Carlos Eduardo', '555.666.777-88', 'carlos.eduardo@exemplo.com', 'cEdu@2025');
 insert into Professor (nome, cpf, email, senha) values ('Fernanda Oliveira', '222.333.444-55', 'fernanda.oliveira@exemplo.com', 'fernanda#1');
+
+insert into VideoAulas (titulo, curso_id, turma_id, professor_id) values ();
+insert into VideoAulas (titulo, curso_id, turma_id, professor_id) values ();
+insert into VideoAulas (titulo, curso_id, turma_id, professor_id) values ();
+insert into VideoAulas (titulo, curso_id, turma_id, professor_id) values ();
+insert into VideoAulas (titulo, curso_id, turma_id, professor_id) values ();
+
+insert into Progresso (disciplina_id, aluno_id) values ();
+insert into Progresso (disciplina_id, aluno_id) values ();
+insert into Progresso (disciplina_id, aluno_id) values ();
+insert into Progresso (disciplina_id, aluno_id) values ();
+insert into Progresso (disciplina_id, aluno_id) values ();
+
+INSERT INTO Aluno (nome, cpf, email, senha) VALUES (João Silva, 123.456.789-00, joao.silva@gmail.com, senha123);
+INSERT INTO Aluno (nome, cpf, email, senha) VALUES (Maria Oliveira, 987.654.321-00, maria.oliveira@gmail.com, senha456);
+INSERT INTO Aluno (nome, cpf, email, senha) VALUES (Carlos Souza, 111.222.333-44, carlos.souza@hotmail.com, senha789);
+INSERT INTO Aluno (nome, cpf, email, senha) VALUES (Ana Costa, 555.666.777-88, ana.costa@yahoo.com, senha321);
+INSERT INTO Aluno (nome, cpf, email, senha) VALUES (Lucas Pereira, 444.555.666-77, lucas.pereira@gmail.com, senha654);
